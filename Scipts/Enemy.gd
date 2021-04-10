8c0b8b4476205842f1b66ef3f201798afa08ceb1
TrueRaider <patrick.dalphy@gmail.com>
2021-04-10 16:57:16 -0400

    couldn't tell you

--- Scipts/Enemy.gd
+++ Scipts/Enemy.gd
@@ -4,9 +4,23 @@ extends Node2D
 export (int) var maxHealth
 onready var health = maxHealth
 export (String) var enemyName
+var velo = Vector2()
+var plPos = Vector2()
+var speed = 100
 
 func dialogue(speech):
 	#Display text box with parameter
 	pass
 	
 #Here is where generic attack projectiles/paterns will be placed, so that individual enemies can reference them
+func _ready():
+	
+	pass
+
+func _process(delta):
+	var Tre = get_parent()
+	plPos = Tre.PlayerPos
+	velo = position - plPos
+	position.x = position.x + (plPos.position.x * speed)
+	position.y = position.y + (plPos.position.y * speed)
+	pass
